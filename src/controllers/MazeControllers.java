6286253ff58540f3c1ae476f886039e68c953da9
David Banegas Lazo <abanegasl@est.ups.edu.ec>
2025-07-17 19:31:20 -0500

    Clase Maze y estructura del proyecto creada

---
+++ src/controllers/MazeControllers.java
@@ -0,0 +1,29 @@
+package controllers;
+
+public class MazeControllers {
+
+    public MazeControllers(boolean[][] predefinedMaze) {
+        
+    }
+
+    public void printMaze() {
+        boolean[][] predefinedMaze = {
+            {true, true, true, true},
+            {false, true, true, true},
+            {true, true, false, false},
+            {true, true, true, true}
+        };
+        for (int i = 0; i < predefinedMaze.length; i++) {
+            for (int j = 0; j < predefinedMaze[i].length; j++) {
+                System.out.print(predefinedMaze[i][j] ? " - " : " * ");
+            }
+            System.out.println();
+        }
+    }
+
+    public boolean[][] getGrid() {
+        return null;
+    }
+    
+
+}
