18d7c0d64b9056c37ee0ede90a3aadc7d5ae3b2a
PRESTEBAN <145607969+PRESTEBAN@users.noreply.github.com>
2025-07-25 14:54:24 -0500

    final

--- src/controllers/MazeControllers.java
+++ src/controllers/MazeControllers.java
@@ -6,24 +6,4 @@ public class MazeControllers {
         
     }
 
-    public void printMaze() {
-        boolean[][] predefinedMaze = {
-            {true, true, true, true},
-            {false, true, true, true},
-            {true, true, false, false},
-            {true, true, true, true}
-        };
-        for (int i = 0; i < predefinedMaze.length; i++) {
-            for (int j = 0; j < predefinedMaze[i].length; j++) {
-                System.out.print(predefinedMaze[i][j] ? " - " : " * ");
-            }
-            System.out.println();
-        }
-    }
-
-    public boolean[][] getGrid() {
-        return null;
-    }
-    
-
 }
